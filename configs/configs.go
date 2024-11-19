f01a8969b994acd805da52c4ddfdd83a12f1bba9
Indra <hiimindra12345@gmail.com>
2024-11-19 23:26:25 +0530

    configs and controller code format

--- configs/configs.go
+++ configs/configs.go
@@ -1,31 +1,31 @@
 package configs
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
 
-    "github.com/joho/godotenv"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func init() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatalf("Error loading .env file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 }
 
 func DatabaseConnection() *gorm.DB {
-    DB_URI := os.Getenv("DATABASE_URI")
-    if DB_URI == "" {
-        panic("DATABASE_URI environment variable is not set")
-    }
+	DB_URI := os.Getenv("DATABASE_URI")
+	if DB_URI == "" {
+		panic("DATABASE_URI environment variable is not set")
+	}
 
-    db, err := gorm.Open(postgres.Open(DB_URI), &gorm.Config{})
-    if err != nil {
-        panic(fmt.Sprintf("failed to connect to database: %v", err))
-    }
-    return db
+	db, err := gorm.Open(postgres.Open(DB_URI), &gorm.Config{})
+	if err != nil {
+		panic(fmt.Sprintf("failed to connect to database: %v", err))
+	}
+	return db
 }
--- controllers/controller.go
+++ controllers/controller.go
@@ -1,104 +1,104 @@
 package controllers
 
 import (
-    "encoding/json"
-    "fmt"
-    "gorm/postgresql/configs"
-    "gorm/postgresql/model"
-    "gorm/postgresql/services"
-    "log"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"gorm/postgresql/configs"
+	"gorm/postgresql/model"
+	"gorm/postgresql/services"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 type Controller interface {
-    NewUser(w http.ResponseWriter, r *http.Request)
-    GetAllUsers(w http.ResponseWriter, r *http.Request)
-    GetUser(w http.ResponseWriter, r *http.Request)
-    DeleteUser(w http.ResponseWriter, r *http.Request)
+	NewUser(w http.ResponseWriter, r *http.Request)
+	GetAllUsers(w http.ResponseWriter, r *http.Request)
+	GetUser(w http.ResponseWriter, r *http.Request)
+	DeleteUser(w http.ResponseWriter, r *http.Request)
 }
 
 type control struct {
-    service services.Service
+	service services.Service
 }
 
 func StartOp() Controller {
-    conn := configs.DatabaseConnection()
-    conn.Table("accounts").AutoMigrate(&model.Person{})
-    mod := model.CreateConntection(conn)
-    service := services.NewService(mod)
-    return &control{
-        service: service,
-    }
+	conn := configs.DatabaseConnection()
+	conn.Table("accounts").AutoMigrate(&model.Person{})
+	mod := model.CreateConntection(conn)
+	service := services.NewService(mod)
+	return &control{
+		service: service,
+	}
 }
 
 func (c *control) NewUser(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-    type person struct {
-        Name   string `json:"name,omitempty"`
-        Age    int    `json:"age,omitempty"`
-        Number string `json:"number,omitempty"`
-    }
-    var account person
-    json.NewDecoder(r.Body).Decode(&account)
+	type person struct {
+		Name   string `json:"name,omitempty"`
+		Age    int    `json:"age,omitempty"`
+		Number string `json:"number,omitempty"`
+	}
+	var account person
+	json.NewDecoder(r.Body).Decode(&account)
 
-    err := c.service.AddNewAccount(account.Name, account.Number, account.Age)
-    if err != nil {
-        log.Fatal(err)
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
+	err := c.service.AddNewAccount(account.Name, account.Number, account.Age)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-    w.WriteHeader(http.StatusAccepted)
-    log.Println("Created New User")
-    result := fmt.Sprintf("Created user %s, %d, %s", account.Name, account.Age, account.Number)
-    json.NewEncoder(w).Encode(result)
+	w.WriteHeader(http.StatusAccepted)
+	log.Println("Created New User")
+	result := fmt.Sprintf("Created user %s, %d, %s", account.Name, account.Age, account.Number)
+	json.NewEncoder(w).Encode(result)
 }
 
 func (c *control) GetAllUsers(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-    users, err := c.service.FindAllAccounts()
-    if err != nil {
-        log.Fatal(err)
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-    }
-    w.WriteHeader(http.StatusAccepted)
-    log.Println("Found all the users")
-    json.NewEncoder(w).Encode(users)
+	users, err := c.service.FindAllAccounts()
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
+	log.Println("Found all the users")
+	json.NewEncoder(w).Encode(users)
 }
 
 func (c *control) GetUser(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-    name := mux.Vars(r)["name"]
-    account, err := c.service.FindAccount(name)
-    if err != nil {
-        log.Fatal(err)
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
+	name := mux.Vars(r)["name"]
+	account, err := c.service.FindAccount(name)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-    w.WriteHeader(http.StatusAccepted)
-    log.Println("Found the User")
-    json.NewEncoder(w).Encode(account)
+	w.WriteHeader(http.StatusAccepted)
+	log.Println("Found the User")
+	json.NewEncoder(w).Encode(account)
 }
 
 func (c *control) DeleteUser(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-    name := mux.Vars(r)["name"]
-    account, err := c.service.DeleteAccount(name)
-    if err != nil {
-        log.Fatal(err)
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
+	name := mux.Vars(r)["name"]
+	account, err := c.service.DeleteAccount(name)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-    w.WriteHeader(http.StatusAccepted)
-    log.Println("Deleted the user")
-    json.NewEncoder(w).Encode(account)
+	w.WriteHeader(http.StatusAccepted)
+	log.Println("Deleted the user")
+	json.NewEncoder(w).Encode(account)
 }
